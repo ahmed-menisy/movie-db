ed806f7d089ddcae72c8cdaa1bd3075c8bac0f5c
ahmed-menisy <amelmaster16@gmail.com>
2022-07-25 13:06:42 +0200

    add footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
+  constructor(private _Renderer2: Renderer2) {}
   title = 'final-movie';
   ngOnInit(): void {
     const nav = document.querySelector('nav');
